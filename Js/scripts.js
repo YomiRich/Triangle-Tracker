48ca12acc126b9f878b6192408b6bed427cdcd2f
Yomi Rich <ymueni@gmail.com>
2019-06-03 10:02:40 +0300

    Add the function on js

---
+++ Js/scripts.js
@@ -0,0 +1,11 @@
+function calculate() {
+  var a parseInt(document.getElementById("a").value);
+  var b parseInt(document.getElementById("b").value);
+  var c parseInt(document.getElementById("c").value);
+  var submit;
+
+  //*----If statements----//*
+  if (true) {
+
+  }
+}
