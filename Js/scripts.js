8048f1c560e94b55f76beab76f89a7a6b6ea33b3
Yomi Rich <ymueni@gmail.com>
2019-06-03 11:00:51 +0300

    Added the conditions using If

--- Js/scripts.js
+++ Js/scripts.js
@@ -4,8 +4,22 @@ function calculate() {
   var c parseInt(document.getElementById("c").value);
   var submit;
 
-  //*----If statements----//*
-  if (true) {
+//Basics of a triangle--->
+//--a+b>=c--//
+//--b+c>=a--//
+//--a+c>=b--//
 
-  }
+  //*----If statements----//*
+  if(((a + b > c) && (b + c > a) && (a + c > b)) && (a !== 0 || b !== 0 || c !== 0)) {
+    if (a === b && b === c && c===a) {
+output.textContent = "Equilateral triangle";}
+ else if(a!== b && a!== c && b!== a && b!== c) {
+output.textcontent = "Scalene Triangle";}
+else if (a === b && a!== c || a === c && a!== b || b === c && b!== c) {
+output.textContent = "I'm an isosceles triangle";
+}
+else {
+  output.textcontent = "Not a triangle"
+}
+
 }
