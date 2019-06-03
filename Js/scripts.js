57a93b5da8d74ca4de0ac202e25601b4bbdfd200
Yomi Rich <ymueni@gmail.com>
2019-06-03 11:28:20 +0300

    Add all changes made

--- Js/scripts.js
+++ Js/scripts.js
@@ -2,7 +2,6 @@ function calculate() {
   var a parseInt(document.getElementById("a").value);
   var b parseInt(document.getElementById("b").value);
   var c parseInt(document.getElementById("c").value);
-  var submit;
 
 //Basics of a triangle--->
 //--a+b>=c--//
@@ -10,16 +9,26 @@ function calculate() {
 //--a+c>=b--//
 
   //*----If statements----//*
-  if(((a + b > c) && (b + c > a) && (a + c > b)) && (a !== 0 || b !== 0 || c !== 0)) {
-    if (a === b && b === c && c===a) {
-output.textContent = "Equilateral triangle";}
- else if(a!== b && a!== c && b!== a && b!== c) {
-output.textcontent = "Scalene Triangle";}
-else if (a === b && a!== c || a === c && a!== b || b === c && b!== c) {
-output.textContent = "I'm an isosceles triangle";
+  if((a + b > c) && (b + c > a) && (a + c > b)) && (a !== 0 || b !== 0 || c !== 0))
+  {
+    if (a === b && b === c && c===a)
+  {
+   alert("Equilateral triangle");
+   }
+   else if(a!== b && a!== c && b!== a && b!== c)
+   {
+   alert( "Scalene Triangle");
+   }
+    else if (a === b && a!== c || a === c && a!== b || b === c && b!== c)
+    {
+    alert("I'm an isosceles triangle");
+    }
+   else
+   {
+  alert("Not a triangle")
+    }
+
 }
-else {
-  output.textcontent = "Not a triangle"
-}
+
 
 }
